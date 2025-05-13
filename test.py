0a35045d29ff6710e9f26d0113aafe78bb015e38
Ramjee Mishra <131847922+ramjeemishra@users.noreply.github.com>
2025-05-13 22:34:14 +0530

    test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ while True:
     if not ret:
         break
     cv2.imshow("Test", frame)
-    if cv2.waitKey(1) == 27:  # ESC to quit
+    if cv2.waitKey(1) == 27:
         break
 
 cap.release()
